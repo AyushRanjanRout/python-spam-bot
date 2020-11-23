31a201946dc7b51fb60687b822cecdc10bda7ec5
Ayush Ranjan Rout <57044707+AyushRanjanRout@users.noreply.github.com>
2020-11-23 20:10:57 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,10 @@
+#Made By Ayush
+import pyautogui 
+import webbrowser as wb
+import time
+wb.open("web.whatsapp.com") #Enter Your Url that you want Like messenger,instagram
+for i in range(1000):
+    pyautogui.press("i love you")
+    pyautogui.press("enter")
+    pyautogui.press("Give Me Your Heart")
+    pyautogui.press("enter")
